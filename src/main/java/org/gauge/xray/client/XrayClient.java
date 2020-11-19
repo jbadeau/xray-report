515012c9270fd5d4165057d2f364ee787d83abcb
Jose Badeau <jose.badeau@gmail.com>
2020-11-19 21:26:54 +0100

    feat: cloud support
    Supports Jira Cloud with API token
    
    BREAKING CHANGE: env properties changed from camel case snake case
    
    12

--- src/main/java/org/gauge/xray/client/XrayClient.java
+++ src/main/java/org/gauge/xray/client/XrayClient.java
@@ -1,6 +1,8 @@
 package org.gauge.xray.client;
 
 import com.google.common.net.HttpHeaders;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import okhttp3.*;
 import org.gauge.xray.Credentials;
 
@@ -12,6 +14,11 @@ import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 public class Client {
+
+    private static Gson gson = new GsonBuilder()
+            .disableHtmlEscaping()
+            .setPrettyPrinting()
+            .create();
 
     public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
 
@@ -34,30 +41,13 @@ public class Client {
         this.credentials = credentials;
     }
 
-    public void authenticate(String baseUrl, String body) throws IOException {
-        Request request = new Request.Builder()
-                .url(baseUrl + "/rest/auth/latest/session")
-                .post(RequestBody.create(body, JSON))
-                .build();
-        try (Response response = client.newCall(request).execute()) {
-            if (!response.isSuccessful()) {
-                throw new RuntimeException(String.format("Failed to authenticate with Jira %s", response.body().string()));
-            }
-        }
-    }
-
     public Response post(String body) throws IOException {
         Request request = new Request.Builder()
                 .url(baseUrl + "/rest/raven/1.0/import/execution")
-                .header(HttpHeaders.AUTHORIZATION, getAuthorizationHeader())
+                .header(HttpHeaders.AUTHORIZATION, credentials.toBase64String())
                 .post(RequestBody.create(body, JSON))
                 .build();
         return client.newCall(request).execute();
     }
-
-    private String getAuthorizationHeader() {
-        byte[] encodedAuth = Base64.getEncoder().encode(credentials.getCredentials().getBytes(StandardCharsets.ISO_8859_1));
-        return "Basic " + new String(encodedAuth);
-    }
-
+    
 }
---
+++ src/test/java/org/gauge/xray/client/ClientTest.java
@@ -0,0 +1,19 @@
+package org.gauge.xray.client;
+
+import okhttp3.Response;
+import org.gauge.xray.Credentials;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class ClientTest {
+
+    @Before
+    public void beforeEachTestMethod() {
+    }
+    
+}
